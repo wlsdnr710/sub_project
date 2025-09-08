2c9721b73ca2866c812f83e610501f2d418de0be
Jinwook Kim <wlsdnr710@naver.com>
2025-09-08 17:07:48 +0900

    navbar

--- frontend/front/src/components/Navbar/index.jsx
+++ frontend/front/src/components/Navbar/index.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useState } from "react";
+import Logo from "./layout/Logo";
+import AuthButtons from "./auth/DAuthButtons";
+import DAuthButtons from "./auth/DAuthButtons";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+function Navbar() {
+  const isAuthenticated = false;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onLoginClick = () => {
+    setIsOpen(true);
+    alert("로그인");
+  };
+
+  const onSignupClick = () => {
+    alert("회원가입");
+  };
+
+  const onLogoutClick = () => {
+    alert("로그아웃");
+  };
+  return (
+    <nav className="bg-purple-500">
+      <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-14 md:h-20">
+          <div className="flex items-center flex-1">
+            <Logo />
+            <DAuthButtons
+              isAuthenticated={isAuthenticated}
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+              onLogoutClick={onLogoutClick}
+              onLoginClick={onLoginClick}
+              onSignupClick={onSignupClick}
+            />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
---
+++ frontend/front/src/components/Navbar/layout/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <Link to="/" className="text-white font-bold text-xl flex-shrink-0">
+      화이팅
+    </Link>
+  );
+};
+
+export default Logo;
